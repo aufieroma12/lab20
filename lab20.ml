620240cff551bced122b1e67a259ef0ee090a5c9
Massimo Aufiero <Massimo@Massimos-MBP-3.hsd1.ma.comcast.net>
2020-04-23 11:40:09 -0400

    lab20 done

--- lab20.ml
+++ lab20.ml
@@ -1,40 +1,64 @@
-type image = float list list ;;
-(* images are lists of lists of floats between 0. (white) and 1. (black) *)
-type size = int * int ;;
 open Graphics ;;
+open List ;;
   
-(* threshold thershold image -- image where pixels above the threshold
+(* threshold threshold image -- image where pixels above the threshold
 value are black *)
-let threshold img threshold =
-  List.map  (fun row -> List.map (fun v -> if v <= threshold then 0. else 1.)
-                                 row) img
-       
-(* show the image *)
-let depict img =
-  Graphics.open_graph ""; Graphics.clear_graph ();
-  let x, y = List.length (List.hd img), List.length img in Graphics.resize_window x y;
-  let depict_pix v r c = let lvl = int_of_float (255. *. (1. -. v)) in Graphics.set_color (Graphics.rgb lvl lvl lvl);
-  plot c (y - r) in
-  List.iteri (fun r row -> List.iteri (fun c pix -> depict_pix pix r c) row) img;
-  Unix.sleep 2; Graphics.close_graph () ;;
 
-(* dither max image -- dithered image *)
-let dither img =
-  List.map
-    (fun row ->
-     List.map
-       (fun v -> if v > Random.float 1.
-                 then 1.
-                 else 0.) row)
-    img
-    
-let mona = Monalisa.image ;;
-  
-  depict mona ;;
-    
-  let mona_threshold = threshold mona 0.75 ;;
-    depict mona_threshold ;;
-      
-    let mona_dither = dither mona ;;
-      depict mona_dither ;;
+module type IMAGES = 
+  sig
+  	type image = float list list 
+	(* images are lists of lists of floats between 0. (white) and 1. (black) *)
+	type size = int * int 
+
+	val map_array : (float -> float) -> image -> image 
+
+	val threshold : image -> float -> image
+
+	val depict : image -> unit
+
+	val dither : image -> image
+  end 
+
+
+module Images : IMAGES = 
+  struct
+  	type image = float list list 
+
+	type size = int * int 
+
+	let map_array f img =
+		map (map f) img ;;
+
+	let threshold img threshold =
+		map_array (fun value -> if value <= threshold then 0. else 1.) img ;;
+	       
+	(* show the image *)
+	let depict img =
+	  open_graph ""; 
+	  clear_graph ();
+	  let x, y = length (hd img), length img 
+	  in resize_window x y;
+	  let depict_pix value row column = 
+	  	let lvl = int_of_float (255. *. (1. -. value)) 
+	    in set_color (rgb lvl lvl lvl);
+		plot column (y - row) in
+	  iteri (fun r row -> iteri (fun c pix -> depict_pix pix r c) row) img;
+	  Unix.sleep 2; 
+	  close_graph () ;;
+
+	(* dither max image -- dithered image *)
+	let dither img =
+	  map_array (fun value -> if value > Random.float 1. then 1. else 0.) img
+  end
+
+
+
+
+let display_images () =
+	let mona = Monalisa.image in
+	Images.depict mona ;
+	Images.depict (Images.threshold mona 0.75);
+	Images.depict (Images.dither mona) ;;
+
+display_images () ;;
            
